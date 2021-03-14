22ed5acef474348d520f70f93493bc5d63b46d60
Audywb <woranat.bo.62@ubu.ac.th>
2021-03-15 00:00:52 +0700

    server fix of name

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {MONGODB} = require('./config');
 const resolvers = require('./graphql/resolvers');
 const typeDefs = require('./graphql/typeDefs');
 const pubsub = new PubSub();
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({
     typeDefs,
